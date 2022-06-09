5da214ac9d9d83d75a2a1db769245c413cc3d01a
Naman Sharma <namansharma232009@gmail.com>
2022-06-09 18:31:28 +0530

    Update text_to_list.py

--- text/text_to_list.py
+++ text/text_to_list.py
@@ -1,2 +1,13 @@
-file = open('text.txt').readlines()
-lst = [line for line in file.strip()]
+"""
+script to get list from a text file 
+without any separator
+""""
+
+def texttolist(filename: str) -> list:
+    with open(filename) as file:
+        lines = file.readlines()
+        lines = [line.rstrip() for line in lines]
+    return lines
+
+print(texttolist("text.txt")
+# output - ["a","b","c"]
